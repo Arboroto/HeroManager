12710feb3852604e54013bcd4b3b6cda83b4edfd
Alberto Martí <af1pb.albertomarti@gmail.com>
2024-10-23 20:10:11 +0200

    Task 7 created page edit-hero

--- src/app/public/public.routes.ts
+++ src/app/public/public.routes.ts
@@ -1,12 +1,25 @@
 import { Routes } from '@angular/router';
 import { PublicComponent } from './public.component';
+import { CreateEditHeroComponent } from './page/create-edit-hero/create-edit-hero.component';
 
 export const PUBLIC_ROUTES: Routes = [
   {
     path: '',
     component: PublicComponent,
     children: [
-      { path: '', redirectTo: '', pathMatch: 'full' } 
-    ]
+        {
+          path: '',
+          redirectTo: 'home',
+          pathMatch: 'full'
+        },
+        {
+          path: 'form/:id',
+          component: CreateEditHeroComponent
+        },
+        {
+          path: 'form',
+          component: CreateEditHeroComponent
+        },
+      ]
   }
 ];
